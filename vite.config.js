736cb410d61f45f34e336a9d692407160e69ab12
Dev.DY <kdydesign@gmail.com>
2023-01-03 18:39:21 +0900

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost'
+    }
+  },
+  plugins: [
+    vue({ template: transformAssetUrls }),
+    quasar({
+      sassVariables: 'src/assets/css/quasar.variables.scss'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
