5d73594ccf5891276ac874edb2679b2d50f37f6f
Dev.DY <kdydesign@gmail.com>
2023-01-03 18:41:13 +0900

    Create LoginForm.vue

---
+++ src/views/LoginForm.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const id = ref(void 0)
+const pwd = ref(void 0)
+const router = useRouter()
+
+function onLogin() {
+  router.push({ path: '/AboutView' })
+}
+</script>
+
+<template>
+  <q-page
+    class="bg-yellow page-a-padding full-width content-center justify-center flex"
+  >
+    <div class="q-mx-auto column q-gutter-y-lg" style="width: 500px">
+      <div class="col">
+        <q-input v-model="id" label="ID" outlined color="red" />
+      </div>
+
+      <div class="col">
+        <q-input v-model="pwd" label="PASSWORD" outlined color="red" />
+      </div>
+
+      <div class="col">
+        <q-btn label="text" color="red" class="fit" @click="onLogin" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<style lang="sass">
+.page-a-padding
+  padding: 0px 40px
+</style>
