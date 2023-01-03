e09d776da592b09b5808b8797547dd88e65edc6a
Dev.DY <kdydesign@gmail.com>
2023-01-03 18:40:13 +0900

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:vue/vue3-recommended',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier'
+	],
+	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	rules: {
+		'vue/multi-word-component-names': 'off',
+		quotes: [ 1, 'single' ],
+		'prettier/prettier': [
+			'error',
+			{
+				semi: false,
+				singleQuote: true,
+				trailingComma: 'none',
+				spaceBeforeFunctionParen: true
+			}
+		]
+	}
+}
