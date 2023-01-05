4d38c457007ef16cdc3a7d635c2eceb30ba6adea
kdydesign <kdydesign@gmail.com>
2023-01-05 22:17:32 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
 
-import Layouts from '@/components/Layouts.vue'
+import Layouts from '@/components/Layouts/Layouts.vue'
 </script>
 
 <template>
