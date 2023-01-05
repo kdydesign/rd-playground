96ba03d9d2176431a830c034fce4f74e0aa980a6
Dev.DY <kdydesign@gmail.com>
2023-01-05 17:24:16 +0900

    Create HeaderTop.vue

---
+++ src/components/Layouts/HeaderTop.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-header class="bg-white text-black">
+    <q-toolbar
+      class="justify-center"
+      style="border-bottom: 1px solid #c6c6c6;height: 70px;"
+    >
+      <q-toolbar-title
+        class="flex justify-center"
+        style="font-size: 28px;font-weight: 600"
+      >
+        Header
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
