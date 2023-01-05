cf0e2b53fa9e967c2c1dd8b74aab808f736b2c2b
Dev.DY <kdydesign@gmail.com>
2023-01-05 17:22:36 +0900

    Create SelectOsiris.vue

---
+++ src/views/SelectOsiris.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-page class="page-a-padding full-width content-center justify-center flex">
+    <div
+      class="q-mx-auto column q-gutter-y-lg"
+      style="width: 500px"
+    >
+      <div class="col">
+        <q-card class="my-card">
+          <q-card-section class="bg-primary text-white">
+            <div class="text-h6">
+              RD
+            </div>
+            <div class="text-subtitle2">
+              2023/11/11
+            </div>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions align="right">
+            <q-btn flat>
+              신청
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+
+      <div class="col">
+        <q-card class="my-card">
+          <q-card-section class="bg-primary text-white">
+            <div class="text-h6">
+              PD
+            </div>
+            <div class="text-subtitle2">
+              2023/11/11
+            </div>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions align="right">
+            <q-btn flat>
+              신청
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+
+      <div class="col">
+        <q-card class="my-card">
+          <q-card-section class="bg-primary text-white">
+            <div class="text-h6">
+              YD
+            </div>
+            <div class="text-subtitle2">
+              2023/11/11
+            </div>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions align="right">
+            <q-btn flat>
+              신청
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
