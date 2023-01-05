6278061069102a89ab9b748976c5058979c71f20
Dev.DY <kdydesign@gmail.com>
2023-01-05 17:21:25 +0900

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,33 @@ module.exports = {
 		'eslint:recommended',
 		'@vue/eslint-config-prettier'
 	],
+	parserOptions: {/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:vue/vue3-recommended',
+		'eslint:recommended'
+	],
 	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	rules: {
+		'vue/multi-word-component-names': 'off',
+		quotes: [ 1, 'single' ]
+		// 'prettier/prettier': [
+		// 	'error',
+		// 	{
+		// 		semi: false,
+		// 		singleQuote: true,
+		// 		trailingComma: 'none',
+		// 		spaceBeforeFunctionParen: true
+		// 	}
+		// ]
+	}
+}
+
 		ecmaVersion: 'latest'
 	},
 	rules: {
