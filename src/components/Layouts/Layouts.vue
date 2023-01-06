9ada75ae8634464ce4ee71ade89dc876d3ff97ca
Dev.DY <kdydesign@gmail.com>
2023-01-06 15:09:22 +0900

    Update Layouts.vue

--- src/components/Layouts/Layouts.vue
+++ src/components/Layouts/Layouts.vue
@@ -2,28 +2,29 @@
 </script>
 
 <template>
-  <div>
-    <q-layout view="lhr Lpr lfr">
-      <q-page-container>
-        <router-view v-slot="{ Component }">
-          <transition
-            enter-active-class="animated fadeIn"
-            leave-active-class="animated fadeOut"
-            mode="out-in"
-          >
-            <component :is="Component" />
-          </transition>
-        </router-view>
-      </q-page-container>
+  <q-layout view="lhr Lpr lfr">
+    <q-page-container>
+      <router-view v-slot="{ Component }">
+        <transition
+          enter-active-class="animated fadeIn"
+          leave-active-class="animated fadeOut"
+          mode="out-in"
+        >
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </q-page-container>
 
-      <q-footer class="bg-white text-grey-5">
-        <q-toolbar class="justify-center">
-          <span>Copyright © 2023 Team R46D,
-            <sup style="font-size: 10px">RD</sup>디와</span>
-        </q-toolbar>
-      </q-footer>
-    </q-layout>
-  </div>
+    <q-footer class="bg-white text-grey-5">
+      <q-toolbar class="justify-center">
+        <div class="copyright flex">
+          <span>Copyright © 2023 Team R46D,</span>
+          <span class="sup">RD</span>
+          <span>디와</span>
+        </div>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
 </template>
 
 <style lang="sass">
@@ -31,4 +32,10 @@
   min-width: 100%
   min-height: 100%
   height: 0px
+
+.copyright
+  font-size: 12px
+  .sup
+    font-size: 10px
+    line-height: 14px
 </style>
