e3822a7258d29547cd0c993ff4bda92e918b0aab
Dev.DY <kdydesign@gmail.com>
2023-01-06 14:30:05 +0900

    Update OsirisReg.vue

--- src/views/OsirisReg.vue
+++ src/views/OsirisReg.vue
@@ -1,12 +1,37 @@
 <script setup>
-import {ref} from 'vue'
+import { ref, onBeforeMount, onUnmounted } from 'vue'
 import { useRouter } from 'vue-router'
+import moment from 'moment'
+
 import PopUp from '@/views/popup/PopUp.vue'
 import HeaderTop from '@/components/Layouts/HeaderTop.vue'
 
 const router = useRouter()
 
 const isPopUpShow = ref(false)
+const regTime = ref(moment())
+const regTimeTxt = ref('-')
+
+let regTimer = void 0
+
+const timer = () => {
+  regTimer = setTimeout(() => {
+    if (regTimer) {
+      clearTimeout(regTimer)
+    }
+    regTime.value = moment(regTime.value).subtract(1, 'seconds')
+    regTimeTxt.value = moment(regTime.value).format('HH:mm:ss')
+    timer()
+  }, 1000)
+}
+
+onBeforeMount(() => {
+  timer()
+})
+
+onUnmounted(() => {
+  clearTimeout(regTimer)
+})
 
 function onRegistration() {
   router.push({ path: '/SelectOsiris' })
@@ -19,26 +44,29 @@ function onOpenList() {
 
 <template>
   <div>
-    <HeaderTop title="Osiris Registration"/>
+    <HeaderTop title="Osiris Registration" />
 
-    <q-page class="page-a-padding full-width content-center justify-center flex">
+    <q-page
+      class="page-a-padding full-width content-center justify-center flex"
+    >
       <div
         class="q-mx-auto column"
         style="width: 500px; text-align: center"
       >
         <div class="col">
           <q-btn
-            size="80px"
+            size="50px"
             round
             label="REG"
             class="r-btn"
             @click="onRegistration"
           />
 
-          <h2>10:22:33</h2>
+          <h2>{{ regTimeTxt }}</h2>
 
-          <div>
-            <span class="float-left">지원현황</span>
+          <div class="rd-container q-pr-md q-pl-md">
+            <span class="rd-sub-title float-left">지원 현황</span>
+
             <table class="q-table">
               <colgroup>
                 <col width="auto">
@@ -48,31 +76,31 @@ function onOpenList() {
 
               <thead>
                 <tr>
-                  <th>RD</th>
-                  <th>PD</th>
-                  <th>YD</th>
+                  <th class="th-head">
+                    RD
+                  </th>
+                  <th class="th-head">
+                    PD
+                  </th>
+                  <th class="th-head">
+                    YD
+                  </th>
                 </tr>
               </thead>
 
               <tbody>
                 <tr>
                   <td>
-                    <q-badge color="green">
-                      <a
-                        href="#"
-                        @click="onOpenList"
-                      >11</a>
-                    </q-badge>
+                    <a
+                      href="#"
+                      @click="onOpenList"
+                    >14</a>
                   </td>
                   <td>
-                    <q-badge color="green">
-                      <a href="#">11</a>
-                    </q-badge>
+                    <a href="#">18</a>
                   </td>
                   <td>
-                    <q-badge color="green">
-                      <a href="#">11</a>
-                    </q-badge>
+                    <a href="#">21</a>
                   </td>
                 </tr>
               </tbody>
@@ -91,4 +119,41 @@ function onOpenList() {
   border-radius: 130px
   background: linear-gradient(270deg, #ED0000 4.5%, #B10000 95.5%) !important
 
+.th-head
+  font-size: 18px !important
+  color: #fff
+
+.q-table
+  box-shadow: 0px 4px 9px rgb(0 0 0 / 11%)
+
+thead
+  background: linear-gradient(314deg, #ED0000 4.5%, #B10000 95.5%) !important
+  border-radius: 10px
+
+th, td
+  border-right: 1px
+  border-color: #adadad
+  border-style: solid
+
+th:first-child
+  border-radius: 6px 0 0 0
+
+th:last-child
+  border-radius: 0 6px 0 0
+
+td:first-child
+  border-radius: 0 0 0 6px
+
+td:last-child
+  border-radius: 0 0 6px 0
+
+td a
+  color: #930000
+  font-weight: bold
+  font-size: 16px !important
+
+.rd-sub-title
+  font-size: 12px
+  margin: 0 0 5px 5px
+  font-weight: 600
 </style>
