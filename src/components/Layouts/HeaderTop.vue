e7fe52d9c2f57268c04fd1b4c92ae99039ef6351
Dev.DY <kdydesign@gmail.com>
2023-01-09 15:24:10 +0900

    Update HeaderTop.vue

--- src/components/Layouts/HeaderTop.vue
+++ src/components/Layouts/HeaderTop.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {defineProps} from 'vue'
+import { defineProps } from 'vue'
 
 defineProps({
   title: String
@@ -10,14 +10,28 @@ defineProps({
   <q-header class="bg-white text-black">
     <q-toolbar
       class="justify-center"
-      style="border-bottom: 1px solid #efefef;height: 70px;"
+      style="border-bottom: 1px solid #efefef; height: 70px"
     >
+      <q-btn
+        icon="arrow_back"
+        flat
+        dense
+        class="navBack"
+        round
+        @click="$router.go(-1)"
+      />
       <q-toolbar-title
         class="flex justify-center"
-        style="font-size: 20px;font-weight: 600"
+        style="font-size: 20px; font-weight: 600"
       >
         {{ title }}
       </q-toolbar-title>
     </q-toolbar>
   </q-header>
 </template>
+
+<style lang="sass">
+.navBack
+  position: absolute
+  left: 10px
+</style>
