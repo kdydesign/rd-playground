25899e686289cf7ab7c9587163d6751d7257b943
Dev.DY <kdydesign@gmail.com>
2023-01-11 17:15:46 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <script setup>
+import { provide } from 'vue'
 import { RouterView } from 'vue-router'
+import { QSpinnerOrbit, useQuasar } from 'quasar'
 
+// components
 import Layouts from '@/components/Layouts/Layouts.vue'
+
+// composable api
+const $q = useQuasar()
+
+// provide $q loader
+provide('$loader', {
+  show: () => {
+    $q.loading.show({
+      spinner: QSpinnerOrbit,
+      spinnerSize: 100,
+      spinnerColor: 'red-10',
+      backgroundColor: 'grey-5',
+    })
+  },
+  hide: () => {
+    $q.loading.hide()
+  }
+})
+
+// provide $q notify
+provide('$notify', {
+  show: (opts) => {
+    $q.notify({ ...opts })
+  }
+})
 </script>
 
 <template>
