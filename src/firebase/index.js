ac04a560de51fd6cafb4ff4a52ce23126e799bc4
Dev.DY <kdydesign@gmail.com>
2023-01-11 17:17:39 +0900

    Create index.js

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyBQaa5h6c3DQvBAu9NZmMUdC6yh0QPBgKE',
+  authDomain: 'support-rd.firebaseapp.com',
+  projectId: 'support-rd',
+  storageBucket: 'support-rd.appspot.com',
+  messagingSenderId: '861511696284',
+  appId: '1:861511696284:web:a02c1fa5570bc61fdbd259',
+  measurementId: 'G-69JKMQ4Y78'
+})
+
+export default firebaseApp
