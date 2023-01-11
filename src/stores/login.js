33b8081f9bc84790cf74ad3a7019a1d31cf2fc60
Dev.DY <kdydesign@gmail.com>
2023-01-11 17:18:08 +0900

    Update login.js

--- src/stores/login.js
+++ src/stores/login.js
@@ -3,10 +3,33 @@ import { ref } from 'vue'
 
 export const loginStore = defineStore('login', () => {
   const userUID = ref(void 0)
+  const name = ref(void 0)
+  const power = ref(void 0)
+  const uid = ref(void 0)
+  const alliance = ref(void 0)
+  const level = ref(void 0)
 
-  function userLogin () {
-    console.log('call axios')
+  function setUserUID (userUUID) {
+    userUID.value = userUUID
   }
 
-  return { userUID, userLogin }
+  function setUserInfo (userInfo, userKey) {
+    userUID.value = userKey
+    name.value = userInfo.name
+    power.value = userInfo.power
+    uid.value = userInfo.uid
+    alliance.value = userInfo.alliance
+    level.value = userInfo.level
+  }
+
+  return {
+    userUID,
+    name,
+    power,
+    uid,
+    alliance,
+    level,
+    setUserInfo,
+    setUserUID
+  }
 })
