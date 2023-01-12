f448177d08ddf3307c48476a0bf55e734dafd0b9
Dev.DY <kdydesign@gmail.com>
2023-01-12 13:46:55 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 import routes from './routes'
 
 const Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+})
+
+Router.beforeEach((to, from) => {
+  return true
 })
 
 export default Router
